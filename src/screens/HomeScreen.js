dce33279140f684f0f5037ec4b4d6a91505da7d6
SachinMhz <sachin.mhz.sm@gmail.com>
2020-02-10 18:43:33 +0545

    showed marker on map after clicking on activity

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,20 +1,24 @@
-import React, {useState, useEffect} from 'react';
+import React, {useState, useEffect, isValidElement} from 'react';
 import {View, StyleSheet} from 'react-native';
 import MapView, {PROVIDER_GOOGLE, Marker, Polyline} from 'react-native-maps';
 import GetLocation from 'react-native-get-location';
+import data from '../data/activityData'; //maile affai banayeko temp-database type where activity coordinates are saved
+import {NavigationEvents} from 'react-navigation';
 
 // create a component
 const Home = () => {
+  const [state, setState] = useState(false); // using this for nth rather that to re-render the screen while changing the tab
+
   const origin = {latitude: 27.672902544858847, longitude: 85.32498076558115}; //patan durbar square
   const destination = {
     latitude: 27.675440333532524,
     longitude: 85.32114688307048,
   }; // chip kinne thaum
 
-  const [marker, setMarker] = useState([]);
-  const [location, setLocation] = useState(null);
-
-  const coordinateArray = [
+  const [marker, setMarker] = useState([]); // this is for showing marker after clicking anywhere on map
+  const [location, setLocation] = useState(null); // variable for getting current location
+ 
+  const coordinateArray = [ // coordinates for drawing polylines
     {latitude: 27.672902544858847, longitude: 85.32498076558115},
     {latitude: 27.673925149842983, longitude: 85.32524932175873},
     {latitude: 27.67425562379755, longitude: 85.32424818724395},
@@ -22,14 +26,13 @@ const Home = () => {
     {latitude: 27.675440333532524, longitude: 85.32114688307048},
   ];
 
-  useEffect(() => {
+  useEffect(() => { 
     GetLocation.getCurrentPosition({
       enableHighAccuracy: true,
       timeout: 15000,
     })
       .then(location => {
-        console.log(location);
-        setLocation(location);
+        setLocation(location); //current location saved after getting location
       })
       .catch(error => {
         const {code, message} = error;
@@ -38,6 +41,11 @@ const Home = () => {
   }, []);
   return (
     <View>
+      <NavigationEvents
+        onWillFocus={() => {
+          setState(!state); //just to re-render when the location is selected and navigation back to homeScreen}*/
+        }}
+      />
       <MapView
         style={styles.map}
         Provider={PROVIDER_GOOGLE}
@@ -52,12 +60,24 @@ const Home = () => {
           latitudeDelta: 0.003,
           longitudeDelta: 0.003,
         }}>
-        {marker.map((marker, i) => (
+
+        {marker.map((
+          marker,
+          i, // add a marker after clicking on map
+        ) => (
           <MapView.Marker key={i} coordinate={marker} />
         ))}
-        <Marker coordinate={origin} title={'some random spot'} />
-        <Marker coordinate={origin} title={'some random spot'} />
-        {location ? (
+
+        {data.map(( // on selecting activity, show correspondning marker on the map
+          item,
+          i, 
+        ) =>
+          item.isSelected ? (
+            <MapView.Marker key={item.id} coordinate={item.coordinates} title={item.title}/>
+          ) : null,
+        )}
+
+        {location ? ( //after GPS on,show our curent location
           <Marker
             coordinate={{
               latitude: location.latitude,
@@ -67,7 +87,9 @@ const Home = () => {
             pinColor={'#003366'}
           />
         ) : null}
-        <Marker coordinate={destination} title={'some random spot'} />
+        <Marker coordinate={origin} title={'Starting pos'} />
+        <Marker coordinate={destination} title={'destination pos'} />
+
         <Polyline
           coordinates={coordinateArray}
           strokeColor="#000" // fallback for when `strokeColors` is not supported by the map-provider
--- src/screens/SplashScreen.js
+++ src/screens/SplashScreen.js
@@ -1,14 +1,23 @@
 //import liraries
-import React from 'react';
-import { View, Text, StyleSheet, Button } from 'react-native';
+import React, { useEffect } from 'react';
+import { View, Text, StyleSheet, Button,Image } from 'react-native';
 
 // create a component
 const SplashScreen = ({navigation}) => {
+    useEffect(()=>{
+        setTimeout(
+            () => { navigation.navigate('Home') }, //showing this screen for 3000 ms period
+            3000
+          )
+    })
     return (
         <View >
              <Text>SplashScreen</Text>
              <Button title="Go to Login" onPress={()=>navigation.navigate('Login')} />
+             <Text>   </Text>
              <Text>email: a  password: a</Text>
+             <Text>   </Text>
+             <Button title="Skip login" onPress={()=>navigation.navigate('Home')} />
         </View>
     );
 };
